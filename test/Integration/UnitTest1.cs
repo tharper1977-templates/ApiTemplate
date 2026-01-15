e9228bdfb4f113c299a1f9a75ab4c85d6db26b7c
tharper1977 <tharper1977@att.net>
2026-01-15 00:11:32 -0600

    Add project files.

---
+++ test/Integration/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace Integration
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
